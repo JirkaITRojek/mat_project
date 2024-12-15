4817a31066311dbf245680f62f9f503ec72817fb
JirkaITRojek <rojek.jiri@zsopava.cz>
2024-12-15 14:44:59 +0100

    Přidání secret.py pro tokeny úprava settings

--- settings.py
+++ settings.py
@@ -1,11 +1,7 @@
 # settings.py
 
-# Bot token
-BOT_TOKEN = "MTI4Nzc2NTg2MDI5OTMwOTExOA.GidfXj.ZtM0wyDt25XUz-ANdwkvkL91iVgh72LRoQKCFw"
-
-# Reddit API přihlašovací údaje
-REDDIT_CLIENT_ID = "h19qqfnBj7HEmmfYHTNOUw"
-REDDIT_CLIENT_SECRET = "QFYhndr7z5AY6IwZ5vWUeaHsq3qKvw"
+# Import tajných klíčů ze souboru secret.py
+from secret import BOT_TOKEN, REDDIT_CLIENT_ID, REDDIT_CLIENT_SECRET
 
 # Výchozí subreddit a typ řazení
 DEFAULT_SUBREDDIT = "memes"
